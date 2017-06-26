789711c3eaf6ad3e783c992810c87c4248768363
Camilo Enrique Rivera <camilo.rive007@gmail.com>
2017-06-26 11:55:59 -0400

    Update Exercise1.java

--- Exercise1.java
+++ Exercise1.java
@@ -1,15 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package exercises1;
+// Exercises 1-11
 
-
-/**
- *
- * @author Camillus
- */
 public class main {
     /**
      * @param args the command line arguments
